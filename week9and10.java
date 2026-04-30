e6dac1eb888a4f698ed19b83fed7999e2687fef4
guttikondasivamadhavareddy-lgtm <guttikondasivamadhavareddy@gmail.com>
2026-04-30 18:11:45 +0530

    level 1 question 3 completed

--- week9and10.java
+++ week9and10.java
@@ -2,19 +2,20 @@ import java.util.Scanner;
 
 public class week9and10 {
 
-    static String createSubstring(String text, int start, int end) {
-        String result = "";
-        for (int i = start; i < end; i++) {
-            result += text.charAt(i);
+    static char[] getChars(String text) {
+        char[] arr = new char[text.length()];
+
+        for (int i = 0; i < text.length(); i++) {
+            arr[i] = text.charAt(i);
         }
-        return result;
+        return arr;
     }
 
-    static boolean compareStrings(String s1, String s2) {
-        if (s1.length() != s2.length()) return false;
+    static boolean compareArrays(char[] a, char[] b) {
+        if (a.length != b.length) return false;
 
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) != s2.charAt(i)) return false;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) return false;
         }
         return true;
     }
@@ -25,19 +26,11 @@ public class week9and10 {
         System.out.print("Enter string: ");
         String text = sc.next();
 
-        System.out.print("Enter start index: ");
-        int start = sc.nextInt();
+        char[] arr1 = getChars(text);
+        char[] arr2 = text.toCharArray();
 
-        System.out.print("Enter end index: ");
-        int end = sc.nextInt();
+        boolean result = compareArrays(arr1, arr2);
 
-        String sub1 = createSubstring(text, start, end);
-        String sub2 = text.substring(start, end);
-
-        boolean result = compareStrings(sub1, sub2);
-
-        System.out.println("Substring using charAt: " + sub1);
-        System.out.println("Substring using built-in: " + sub2);
-        System.out.println("Both are equal: " + result);
+        System.out.println("Arrays are equal: " + result);
     }
 }
