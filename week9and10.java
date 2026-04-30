2c4a29b20f367aace0f0c23b296faf6dcc20afcf
guttikondasivamadhavareddy-lgtm <guttikondasivamadhavareddy@gmail.com>
2026-04-30 18:13:28 +0530

    level 1 question 4 completed

--- week9and10.java
+++ week9and10.java
@@ -1,36 +1,27 @@
-import java.util.Scanner;
-
 public class week9and10 {
 
-    static char[] getChars(String text) {
-        char[] arr = new char[text.length()];
-
-        for (int i = 0; i < text.length(); i++) {
-            arr[i] = text.charAt(i);
-        }
-        return arr;
+    // Method to generate exception
+    static void generateException() {
+        String text = null;
+        System.out.println(text.length()); // ❌ NullPointerException
     }
 
-    static boolean compareArrays(char[] a, char[] b) {
-        if (a.length != b.length) return false;
-
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] != b[i]) return false;
+    // Method to handle exception
+    static void handleException() {
+        try {
+            String text = null;
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("Exception handled: " + e);
         }
-        return true;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter string: ");
-        String text = sc.next();
+        // Uncomment this to see exception crash
+        // generateException();
 
-        char[] arr1 = getChars(text);
-        char[] arr2 = text.toCharArray();
-
-        boolean result = compareArrays(arr1, arr2);
-
-        System.out.println("Arrays are equal: " + result);
+        // Correct way (handled)
+        handleException();
     }
 }
